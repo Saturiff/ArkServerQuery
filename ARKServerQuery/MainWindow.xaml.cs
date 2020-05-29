c44a8b665829ff1f3215c3cb5107bba54ad7aab4
reina42689 <a112231198@gmail.com>
2020-05-29 18:20:14 +0800

    新增縮放查詢界面功能

--- ARKServerQuery/MainWindow.xaml.cs
+++ ARKServerQuery/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Shapes;
 using Localization = ARKServerQuery.Classes.Localization;
 
 namespace ARKServerQuery
@@ -98,6 +99,79 @@ namespace ARKServerQuery
             object serverInfoObject = ((Button)sender).CommandParameter;
 
             watchdog.UpdateWatchList((ServerInfo)serverInfoObject);
+        }
+
+        #endregion
+
+        #region ResizeWindows
+
+        bool ResizeInProcess = false;
+
+        private void Resize_Init(object sender, MouseButtonEventArgs e)
+        {
+            Rectangle senderRect = sender as Rectangle;
+            if (senderRect != null)
+            {
+                ResizeInProcess = true;
+                senderRect.CaptureMouse();
+            }
+        }
+
+        private void Resize_End(object sender, MouseButtonEventArgs e)
+        {
+            Rectangle senderRect = sender as Rectangle;
+            if (senderRect != null)
+            {
+                ResizeInProcess = false; ;
+                senderRect.ReleaseMouseCapture();
+            }
+        }
+
+        private void Resizeing_Form(object sender, MouseEventArgs e)
+        {
+            if (ResizeInProcess)
+            {
+                Rectangle senderRect = sender as Rectangle;
+
+                Window mainWindow = senderRect.Tag as Window;
+
+                if (senderRect != null)
+                {
+                    double width = e.GetPosition(mainWindow).X;
+                    double height = e.GetPosition(mainWindow).Y;
+
+                    senderRect.CaptureMouse();
+
+                    if (senderRect.Name.ToLower().Contains("right"))
+                    {
+                        width += 5;
+                        if (width > 0)
+                            mainWindow.Width = width;
+                    }
+                    if (senderRect.Name.ToLower().Contains("left"))
+                    {
+                        width -= 5;
+                        mainWindow.Left += width;
+                        width = mainWindow.Width - width;
+                        if (width > 0)
+                            mainWindow.Width = width;
+                    }
+                    if (senderRect.Name.ToLower().Contains("bottom"))
+                    {
+                        height += 5;
+                        if (height > 0)
+                            mainWindow.Height = height;
+                    }
+                    if (senderRect.Name.ToLower().Contains("top"))
+                    {
+                        height -= 5;
+                        mainWindow.Top += height;
+                        height = mainWindow.Height - height;
+                        if (height > 0)
+                            mainWindow.Height = height;
+                    }
+                }
+            }
         }
 
         #endregion
